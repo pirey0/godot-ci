a50319d31d51a31b7107b53a45495b753c29f16e
Luca Martinelli <lucxmangajet@gmail.com>
2024-09-10 15:40:26 +0200

    updated output to new api

--- action.sh
+++ action.sh
@@ -25,14 +25,13 @@ chmod +x $GITHUB_WORKSPACE/$7
 $GITHUB_WORKSPACE/$7 --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
 echo "Build Done"
 
-echo ::set-output name=build::build/${SubDirectoryLocation:-""}
-
+echo build=build/${SubDirectoryLocation:-""} >> $GITHUB_OUTPUT
 if [ "$4" = "true" ]
 then
     echo "Packing Build"
     mkdir -p $GITHUB_WORKSPACE/package
     cd $GITHUB_WORKSPACE/build
     zip $GITHUB_WORKSPACE/package/artifact.zip ${SubDirectoryLocation:-"."} -r
-    echo ::set-output name=artifact::package/artifact.zip
+    echo artifact=package/artifact.zip >> $GITHUB_OUTPUT
     echo "Done"
 fi
